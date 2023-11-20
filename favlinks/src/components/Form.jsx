6072ac0925fbfbef2a4d062da339544a69f4e99c
Shohag <shoislamNYC@gmail.com>
2023-11-20 09:12:56 -0500

    Started the form Component and made it render

--- favlinks/src/components/Form.jsx
+++ favlinks/src/components/Form.jsx
@@ -1,8 +1,16 @@
 function Form(){
     return(
-        <div>
-            <form action=""></form>
-        </div>
+        <form action="">
+                <label htmlFor="linkName">Link Name:</label>
+                <br />
+                <input type="text" id="linkName" name="linkName" value=""/>
+                <br />
+                <label htmlFor="url">URL</label>
+                <br />
+                <input type="text" id="url" name="url" value=""/>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
        
     )
 }
